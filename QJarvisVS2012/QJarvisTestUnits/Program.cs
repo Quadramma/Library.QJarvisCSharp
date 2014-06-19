964d3c0e5e9e26e4f2707fd48be002636553b1d5
Javi <javi@quadramma.com>
2014-06-19 15:07:19 -0300

    FileUpload added

---
+++ QJarvisVS2012/QJarvisTestUnits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using com.quadramma.qjarvis.http;
+using Newtonsoft.Json.Linq;
+
+namespace QJarvisTestUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = QJWebClient.GetFileByteArray("asdasdas.jpg");
+            string url = "http://www.quadramma.com/pruebas/qjarvis/api/file/upload";
+            QJUploadFileResponse response = QJWebClient.UploadFile(url, "fileUpload", "asdasdas.jpg", bytes, null);
+            Console.WriteLine(response.message);
+            Console.WriteLine(response.url);
+            Console.WriteLine(response.ok);
+        }
+    }
+}
